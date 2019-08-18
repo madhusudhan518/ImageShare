baa9008e8142800549d132c31ab8c9b1e0c0b522
madhusudhan518 <pvenkatamadhusudhanarao@gmail.com>
2019-08-18 11:48:31 +0530

    added authentication to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_155007) do
+ActiveRecord::Schema.define(version: 2019_08_18_052640) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
