87a213f135d7df34427bce5f686fb7877e9cd58c
madhusudhan518 <pvenkatamadhusudhanarao@gmail.com>
2019-08-19 01:17:51 +0530

    modified homepage view and album form

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :user
-  has_many :photos, dependent: :destroy, counter_cache: true
+  has_many :photos, dependent: :destroy
 
   validates :name, presence: true
 end
